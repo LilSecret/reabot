b792ea488964c7646d4fab76d7c48f448f8c235c
lilsecret <chuckm248@gmail.com>
2024-08-01 12:37:58 -0600

    add react-hot-toast to the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./css/reset.css";
 import "./css/general.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster />
     <App />
   </React.StrictMode>
 );
