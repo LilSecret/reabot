5af21b1a3c4857ce5eb2aff29bb5e31d2a0d3b91
lilsecret <chuckm248@gmail.com>
2024-08-06 18:40:20 -0600

    add address, formRequest, and useInfo types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,25 @@
 export type TMessageType = "Bot" | "User";
 
+export type TAddress = {
+  address1: string;
+  address2: string;
+};
+
+export type TUserInfo = {
+  name: string;
+  email?: string;
+};
+
+export type addressLookups = TAddress[];
+
+export type TFormRequest =
+  | "SetPersonalInfo"
+  | "PropertyLookup"
+  | "AreaPOI"
+  | "Chat"
+  | "Joke"
+  | "Parser";
+
 export type TMessage = {
   type: TMessageType;
   content: string;
